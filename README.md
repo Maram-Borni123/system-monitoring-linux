e8dccbe39cf06df6a29b44df3fdf9142150bdb1e
Maram-Borni123 <maramborni@outlook.com>
2025-10-14 08:29:38 -0700

    modifications

---
+++ README.md
@@ -0,0 +1,19 @@
+##Linux System Monitor :
+
+A real-time Linux system monitor script that tracks CPU, Memory, and Disk usage with alerts and logging.
+
+##features:
+
+- Real-time monitoring
+- Color-coded alerts
+- Customizable thresholds
+- Logs usage to `system_monitor.log`
+
+##Usage:
+bash
+
+##you can run it with default threashold via : 
+./system_monitor.sh
+
+#run it with custom thresholds :
+./system_monitor.sh cpu_limit mem_limit disk_limit
---
+++ system-monitor.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+cpu_limit=${1:-70}
+mem_limit=${1:-80}
+disk_limit=${1:-80}
+
+log_file="system-monitor.log"
+
+print_alert() { echo -e "\e[31m  $1\e[0m"; }
+print_info() { echo -e "\e[32m$1\e[0m"; }
+print_warn() { echo -e "\e[33m$1\e[0m"; }      
+
+while true; do
+    clear
+    echo "        LINUX SYSTEM MONITOR         "
+    echo "Thresholds -> CPU: $cpu_limit%, MEM: $mem_limit%, DISK: $disk_limit%"
+
+    CPU=$(top -bn1 | grep "Cpu(s)" | awk '{print int($2 + $4)}')
+    echo -n "CPU Usage: $CPU% "
+    [ "$CPU" -gt "$cpu_limit" ] && print_alert "high!!!!!!!!"
+    [ "$CPU" -le " $cpu_limit" ] && print_info "✔ Normal"
+
+    MEM=$(free | grep Mem | awk '{print int($3/$2 * 100)}')
+    echo -n "Memory Usage: $MEM% "
+    [ "$MEM" -gt "$mem_limit" ] && print_alert "high!!!!!!!!!"
+    [ "$MEM" -le "$mem_limit" ] && print_info "✔ Normal"
+
+    DISK=$(df / | tail -1 | awk '{print int($5)}')
+    echo -n "Disk Usage: $DISK% "
+    [ "$DISK" -gt "$disk_limit" ] && print_alert "High!!!!!!!"
+    [ "$DISK" -le "$disk_limit" ] && print_info "✔ Normal"
+
+    echo "$(date): CPU=$CPU%, MEM=$MEM%, DISK=$DISK%" >> "$log_file"
+
+    read -p "Do you want to exit? (y/n): " choice
+    if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
+        echo "Exiting system monitor..."
+        break   
+    else  
+        sleep 2
+    fi
+ 
+done

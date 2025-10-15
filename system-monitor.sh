ddf7358ae3719b47609c171c51f469bf16b7b798
Maram-Borni123 <maramborni@outlook.com>
2025-10-14 17:42:41 -0700

    updates Tue 14 Oct 2025 05:42:41 PM PDT

--- system-monitor.sh
+++ system-monitor.sh
@@ -2,49 +2,40 @@
 cpu_limit=${1:-70}
 mem_limit=${1:-80}
 disk_limit=${1:-80}
-technical_dep_email="maramborni@outlook.com"
 log_file="system-monitor.log"
 
 print_alert() { echo -e "\e[31m  $1\e[0m"; }
 print_info() { echo -e "\e[32m$1\e[0m"; }
 print_warn() { echo -e "\e[33m$1\e[0m"; }      
-send_email_alert() {
-    subject="$1"
-    body="$2"
-    echo "$body" | mail -s "$subject" "$technical_dep_email"
-}
 
 while true; do
-    clear
+    touch log_file
     echo "        LINUX SYSTEM MONITOR         "
     echo "Thresholds -> CPU: $cpu_limit%, MEM: $mem_limit%, DISK: $disk_limit%"
 
     CPU=$(top -bn1 | grep "Cpu(s)" | awk '{print int($2 + $4)}')
     echo -n "CPU Usage: $CPU% "
-    if [ "$CPU" -gt "$cpu_limit" ]; then
+    if (( CPU > cpu_limit )); then
     print_alert "CPU High!"
-    send_email_alert "CPU Alert" "CPU usage is $CPU%, above $cpu_limit%!"
 
- else  [ "$CPU" -le " $cpu_limit" ] && print_info "✔ Normal"
+ else  (( "$CPU" < " $cpu_limit" )) && print_info "✔ Normal"
 
 fi
 
     MEM=$(free | grep Mem | awk '{print int($3/$2 * 100)}')
     echo -n "Memory Usage: $MEM% "
-    if [ "$MEM" -gt "$mem_limit" ]; then
+    if (( "$MEM" > "$mem_limit" )) ; then
     print_alert "High Mem huryy !"
-    send_email_alert "Mem alert" "Mem usage is $MEM%, above $mem_limit%!"
 
-   else  [ "$MEM" -le "$mem_limit" ] && print_info "✔ Normal"
+   else  (("$MEM" < "$mem_limit" )) && print_info "✔ Normal"
 fi
 
     DISK=$(df / | tail -1 | awk '{print int($5)}')
     echo -n "Disk Usage: $DISK% "
-    if [ "$DISK" -gt "$disk_limit" ]; then
+    if (("$DISK" > "$disk_limit" )); then
     print_alert "disk about to explodeee"
-    send_email_alert "DISKK Alert" "disk usage is $DISK%, above $disk_limit%!"
 
-    else [ "$DISK" -le "$disk_limit" ] && print_info "✔ Normal"
+    else (( "$DISK" < "$disk_limit" )) && print_info "✔ Normal"
 fi
 
     echo "$(date): CPU=$CPU%, MEM=$MEM%, DISK=$DISK%" >> "$log_file"
@@ -59,5 +50,5 @@ fi
     else  
         sleep 2
     fi
- 
+
 done
